3d7390dffe804eaac6751b11be72d185a37345e9
dongz <dongz.vip@gmail.com>
2017-04-18 00:02:30 -0400

    Added faked orders to database

--- private/data/mongo.bat
+++ private/data/mongo.bat
@@ -2,3 +2,4 @@ mongoimport --mode upsert --db meteor --collection products --port=3001 --file .
 mongoexport --db meteor --collection products --port=3001 --out products_full.json
 mongoimport --mode upsert --db meteor --collection categories --port=3001 --file ./private/data/categories.json
 mongoimport --mode upsert --db meteor --collection retailers --port=3001 --file ./private/data/retailers.json
+mongoimport --mode upsert --db meteor --collection orders --port=3001 --file ./private/data/orders.json

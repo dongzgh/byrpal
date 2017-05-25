d94ed395e295da27496fa64ffc79f49666743b36
dongz <dongz.vip@gmail.com>
2017-05-24 22:44:34 -0400

    Enabled combined orders information

--- client/scripts/controllers/orders.js
+++ client/scripts/controllers/orders.js
@@ -2,15 +2,22 @@ import Moment from 'moment';
 import { Controller } from 'angular-ecmascript/module-helpers';
 
 // Data.
-import { Orders } from '../../../lib/collections';
+import { Products, Orders } from '../../../lib/collections';
 
 export default class OrdersCtrl extends Controller {
   constructor() {
     super(...arguments);
+    this.subscribe('products');
     this.subscribe('orders');
+    this.fullOrders = [];
     this.helpers({
       orders() {
-        return Orders.find({});
+        let dataOrders = Orders.find({}).fetch();
+        dataOrders.forEach(function (dataOrder) {
+          let dataProduct = Products.find({ "_id": dataOrder.productId }).fetch();
+          dataOrder.pictures = dataProduct[0].pictures;
+        })
+        return dataOrders;
       }
     });
   }

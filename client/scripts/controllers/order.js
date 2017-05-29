a0dba36c1dcdcad3b3194ee152c5ff84d57f49e4
dongz <dongz.vip@gmail.com>
2017-05-28 23:55:37 -0400

    Changed order page (not completed)

--- client/scripts/controllers/order.js
+++ client/scripts/controllers/order.js
@@ -2,7 +2,7 @@ import Moment from 'moment';
 import { Controller } from 'angular-ecmascript/module-helpers';
 
 // Data.
-import { Categories, Retailers } from '../../../lib/collections';
+import { Order } from '../../../lib/collections';
 
 export default class OrderCtrl extends Controller {
   // Construction.
@@ -10,17 +10,15 @@ export default class OrderCtrl extends Controller {
     super(...arguments);
 
     // Fields.
-    this.client = "Tom";
-    this.name = "Jamieson Kids Chewable Vitamin D 400 IU Strawberry Tablets";
-    this.quantity = 1;
+    this.subscribe('order');
+    this.client = 'Tom';
     this.totalPrice = 0;
-    this.realUnitPrice = 0;
-    this.realTransportationFee = 0;
-    this.realExpressFee = 0;
-    this.status = "pending";
 
     // Helpers.
     this.helpers({
+      items(){
+        return Order.find({});
+      }
     });
   };
 }
--- client/scripts/controllers/products.js
+++ client/scripts/controllers/products.js
@@ -2,17 +2,32 @@ import Moment from 'moment';
 import { Controller } from 'angular-ecmascript/module-helpers';
 
 // Data.
-import { Products } from '../../../lib/collections';
+import { Products, Order } from '../../../lib/collections';
 
 export default class ProductsCtrl extends Controller {
   constructor() {
     super(...arguments);
     this.subscribe('products');
+    this.subscribe('order');
     this.helpers({
       products() {
         return Products.find({});
       }
     });
+  };
+
+  order(product) {
+    let item = Order.find({ "productId": product._id }).fetch();
+    if (item.length === 0) {
+      Order.insert({
+        "productId": product._id,
+        "name": product.name,
+        "picture": product.pictures[0],
+        "quantity": 1
+      })
+    } else {
+      console.log("update record!")
+    }
   }
 }
 

250d04c7da121641d102e4ed2d793e1b24fc8d09
dongz <dongz.vip@gmail.com>
2017-06-08 23:19:58 -0400

    Updated products and orders collection data

--- private/data/mongo.bat
+++ private/data/mongo.bat
@@ -1,3 +1,4 @@
 mongoimport --mode upsert --db meteor --collection products --port=3001 --file ./private/data/products.json
-mongoexport --db meteor --collection products --port=3001 --out products_full.json
+mongoexport --db meteor --collection products --port=3001 --out products.json
 mongoimport --mode upsert --db meteor --collection orders --port=3001 --file ./private/data/orders.json
+mongoexport --db meteor --collection orders --port=3001 --out orders.json
